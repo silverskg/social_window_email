fd37a044dd31952898851166dd8ac785f35a3108
silverskg <chokichoki_gm@i.softbank.jp>
2021-12-12 19:28:14 +0900

    error message

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -4,8 +4,20 @@ class InquiriesController < ApplicationController
   end
 
   def confirm
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+      render "confirm"
+    else
+      render :new
+    end
   end
 
   def thanks
+  end
+  
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :relationship, :content)
   end
 end
