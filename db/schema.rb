375c81752654a2e96175f12a77b4ba7c1fe0abee
silverskg <chokichoki_gm@i.softbank.jp>
2021-12-15 11:04:38 +0900

    環境変数変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_212_100_026) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2021_12_12_100026) do
 
-  create_table 'inquiries', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'relationship'
-    t.string 'content'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "relationship"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
